615facf6daf65ef57243214cb24a1e731979a161
Killian ROYANT <killianr479@gmail.com>
2022-10-21 11:29:09 +0200

    oups mauvaise branche

--- script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    
-    $(".new-box").on("click", function () {
-        $(".grid").append('<div class="image-box"><img src="https://www.nasa.gov/sites/default/files/styles/full_width/public/thumbnails/image/stsci-01gfnn3pwjmy4rqxkz585bc4qh.png?itok=Xja4XWS0" alt="image"></div>');
-    });
-
-});
